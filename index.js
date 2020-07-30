5c91448d958bac60f5a7a0872cf0fa833e32199c
Brian <shidoitsukaa@gmail.com>
2020-07-31 01:12:12 +0700

    beautified

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const wa = require("@open-wa/wa-automate");
-const fs = require("fs")
+const fs = require("fs");
 const prefix = "!";
 const availableCommands = new Set();
 
 fs.readdir("./commands", (e, files) => {
-    if (e) return console.error(e);
-    files.forEach(commandFile => {
-        availableCommands.add(commandFile.replace(".js", ""));
-    });
+  if (e) return console.error(e);
+  files.forEach((commandFile) => {
+    availableCommands.add(commandFile.replace(".js", ""));
+  });
 });
 
-wa.create().then(bot => start(bot));
+wa.create().then((bot) => start(bot));
 
 function start(bot) {
-    bot.onMessage(async msg => {
-        if (!msg.body.startsWith(prefix)) return;
-        const args = msg.body.slice(prefix.length).trim().split(/ +/g);
-        const command = args.shift().toLowerCase();
+  bot.onMessage(async (msg) => {
+    if (!msg.body.startsWith(prefix)) return;
+    const args = msg.body.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-        if (availableCommands.has(command)) require(`./commands/${command}`).run(msg, bot);
-
-    });
+    if (availableCommands.has(command))
+      require(`./commands/${command}`).run(msg, bot, args);
+  });
 }
